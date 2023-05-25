4cdab524bacaec595f2b532b29cd3dac99e6001c
Milyagami <129770117+Milyagami@users.noreply.github.com>
2023-05-25 15:24:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from functions import *
 def menu():
     print("\n==== Programming Quotes ====")
     print("1. Random quote")
-    print("2. All quote")
+    print("2. Display count")
     print("3. Add quote")
     print("4. Exit")
 
@@ -12,20 +12,21 @@ def main():
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-4) : ")
+        choice = input("Chose your an action(1-4) : ")
         
         if choice == "1":
             print_quote(random_quote(quotes))
         elif choice == "2":
-            view_quotes(quotes)
+            count = int(input("how many qote u want to add ? "))
+            display_quotes(quotes, count)
         elif choice == "3":
             add_quote(quotes, "quotes.txt")
-            print("Quote added !")
+            print("Quote added!")
         elif choice == "4":
             print("Good bye...")
             break
         else:
-            print("Invalid input")
+            print("Wrong input")
 
 if __name__ == "__main__":
     main()
