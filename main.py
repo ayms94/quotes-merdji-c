468391eff28b7cfac0c7f8aec16139a44d2f74c4
Milyagami <129770117+Milyagami@users.noreply.github.com>
2023-05-25 15:14:49 +0200

    Update main.py
    3

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ from functions import *
 def menu():
     print("\n==== Programming Quotes ====")
     print("1. Random quote")
-    print("2. All quotes")
-    print("3. Exit")
+    print("2. All quote")
+    print("3. Add quote")
+    print("4. Exit")
 
 def main():
     while True:
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-3): ")
+        choice = input("Choose your an action (1-4) : ")
         
         if choice == "1":
             print_quote(random_quote(quotes))
         elif choice == "2":
             view_quotes(quotes)
         elif choice == "3":
+            add_quote(quotes, "quotes.txt")
+            print("Quote added !")
+        elif choice == "4":
             print("Good bye...")
             break
         else:
